1d625f5668a8781cfa8ee39a17634eff4f137ef5
Simon Sinding <sinding2000@gmail.com>
2021-03-29 21:50:10 +0200

    Revert "Feature/fix suggestions"

--- src/InvalidAPIKeyError.ts
+++
@@ -1,7 +0,0 @@
-import { FeathersError } from "@feathersjs/errors";
-
-export class InvalidAPIError extends FeathersError {
-  constructor() {
-    super("Invalid API key", "InvalidAPIKey", 401, "InvalidAPIKey", {});
-  }
-}
